e0b89507e74027234bd576dee30ae220f200f59f
ve3 <kawewong@gmail.com>
2022-12-26 00:52:55 +0700

    Remove row meta.

--- App/Controllers/Admin/PluginMetaAndLinks.php
+++ App/Controllers/Admin/PluginMetaAndLinks.php
@@ -48,35 +48,7 @@ if (!class_exists('\\RdEvents\\App\\Controllers\\Admin\\PluginMetaAndLinks')) {
             // add filter action links. this will be displayed in actions area of plugin page. for example: xxxActionLinksBefore | Activate | Edit | Delete | xxxActionLinksAfter
             add_filter('plugin_action_links', [&$this, 'actionLinks'], 10, 5);
             // add filter to row meta. (in plugin page below description) Version xx | By xxx | View details | xxxRowMetaxxx | xxxRowMetaxxx
-            add_filter('plugin_row_meta', [$this, 'rowMeta'], 10, 2);
         }// registerHooks
-
-
-        /**
-         * add links to row meta that is in plugin page under plugin description.
-         * 
-         * @access protected Do not access this method directly. It is for callback only.
-         * @staticvar string $plugin the plugin file name.
-         * @param array $links current meta links
-         * @param string $file the plugin file name for checking.
-         * @return array return modified links.
-         */
-        public function rowMeta($links, $file)
-        {
-            static $plugin;
-            
-            if (!isset($plugin)) {
-                $plugin = plugin_basename(RDEVENTS_FILE);
-            }
-            
-            if ($plugin === $file) {
-                $new_link[] = '<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=9HQE4GVV4KTZE" target="donate_rundiz">' . __('Donate', 'rd-events') . '</a>';
-                $links = array_merge($links, $new_link);
-                unset($new_link);
-            }
-            
-            return $links;
-        }// rowMeta
 
 
     }
